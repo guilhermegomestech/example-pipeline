77eefa97a3e2fc32d3f25d23ae1b0355ff13a6d4
Guilherme Gomes <guilhermeg.contato@gmail.com>
2023-03-14 20:40:23 -0300

    commit initial

---
+++ src/main/java/workpipeline/WorkPipelineApplication.java
@@ -0,0 +1,13 @@
+package workpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPipelineApplication.class, args);
+	}
+
+}
