e4fbe291dd5a977cf5a65d4dfa0b5cc306736b9f
Guilherme Gomes <guilhermeg.contato@gmail.com>
2023-04-02 21:22:15 -0300

    criando logs na aplicação

--- src/main/java/workpipeline/WorkPipelineApplication.java
+++ src/main/java/workpipeline/WorkPipelineApplication.java
@@ -3,10 +3,13 @@ package workpipeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class WorkPipelineApplication {
-
+	public static final Logger LOGGER = Logger.getLogger(WorkPipelineApplication.class.getName());
 	public static void main(String[] args) {
+		LOGGER.info("Iniciando aplicação WorkPipeline...");
 		SpringApplication.run(WorkPipelineApplication.class, args);
 	}
 
--- src/main/java/workpipeline/controllers/PipelineApiController.java
+++ src/main/java/workpipeline/controllers/PipelineApiController.java
@@ -4,14 +4,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import workpipeline.WorkPipelineApplication;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("api/v1/continuous-integration")
 public class PipelineApiController {
 
+    public static final Logger LOGGER = Logger.getLogger(PipelineApiController.class.getName());
     @GetMapping("/msg-sucess")
     public ResponseEntity getSucessMsg(){
-
+        LOGGER.info("Chamando mensagem de sucesso...");
         return ResponseEntity.ok("CRIANDO PIPELINE");
     }
 }
